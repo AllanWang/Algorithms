28403d68b971d71062da1ea62b0c3e3c585a8a4d
Allan Wang <me@allanwang.ca>
2017-10-27 18:27:51 -0400

    Fix expression

--- regular_expression_matching/regex.ml
+++ regular_expression_matching/regex.ml
@@ -1,23 +1,22 @@
 exception Invalid of string
 
 let isMatch input regex =
-  let debug = true in
   let trim s' = if String.length s' > 1 then String.sub s' 1 (String.length s' - 1) else "" in
   let isEmpty s' = String.length s' = 0 in
   let isCharMatch input' regex' = if regex' = '.' then true else input' = regex' in
   let rec isMatch' input' regex' cache' = if isEmpty input' then isEmpty regex' || regex' = "*"
     else if isEmpty regex' then isEmpty input'
     else
-      (* print_string ("Input " ^ input' ^ "; Regex " ^ regex') *)
       let i = String.get input' 0 in
       let r = String.get regex' 0 in
       match r with
       | '*' ->
         (match cache' with
           | None -> raise (Invalid "Cannot start regex with '*'")
-          | Some c -> if not (isCharMatch i c) then false
-            else isMatch' (trim input') (trim regex') cache' || isMatch' (trim input') regex' cache') || isMatch' input' (trim regex') cache'
-      | _ -> if not (isCharMatch i r) then false else isMatch' (trim input') (trim regex') (Some r)
+          | Some c -> if not (isCharMatch i c) then isMatch' input' (trim regex') cache'
+            else isMatch' (trim input') (trim regex') cache' || isMatch' (trim input') regex' cache')
+      | _ -> if not (isCharMatch i r) && (String.length regex' < 2 || String.get regex' 1 <> '*') then false
+        else isMatch' (trim input') (trim regex') (Some r)
   in
   isMatch' input regex None
 
